4c2e909f1a0acd5486e58ae97ed115e4a4f393ce
Alexander Vostres <alex.vostres@gmail.com>
2017-09-19 01:13:25 +0300

    Minor update

--- src/Zip.NetStandard/Crypto/PBKDF2Cng.cs
+++ src/Zip.NetStandard/Crypto/PBKDF2Cng.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using System.Text;
+using System.Security;
 
 namespace Zip.NetStandard.Crypto
 {
@@ -10,9 +9,13 @@ namespace Zip.NetStandard.Crypto
         const int BCRYPT_ALG_HANDLE_HMAC_FLAG = 0x00000008;
         const int BCRYPT_HASH_REUSABLE_FLAG   = 0x00000020;
 
+        [SuppressUnmanagedCodeSecurity]
         [DllImport("bcrypt.dll", CallingConvention = CallingConvention.Winapi)]
-        private static extern unsafe int BCryptDeriveKeyPBKDF2(IntPtr hPrf, byte* pbPassword, int cbPassword, byte* pbSalt, int cbSalt, long cIterations, byte * pbDerivedKey, int cbDerivedKey, int dwFlags = 0);
+        private static extern unsafe int BCryptDeriveKeyPBKDF2([In] IntPtr hPrf, [In] byte* pbPassword,
+            [In] int cbPassword, [In] byte* pbSalt, [In] int cbSalt, [In] long cIterations, [In] byte* pbDerivedKey,
+            [In] int cbDerivedKey, [In] int dwFlags = 0);
 
+        [SuppressUnmanagedCodeSecurity]
         [DllImport("bcrypt.dll", CallingConvention = CallingConvention.Winapi)]
         private static extern int BCryptOpenAlgorithmProvider(
             [Out] out IntPtr phAlgorithm,
@@ -20,11 +23,11 @@ namespace Zip.NetStandard.Crypto
             [In, MarshalAs(UnmanagedType.LPWStr)] string pszImplementation, 
             [In] int dwFlags);
 
-        private static IntPtr hmacSha1Handle;
+        private static readonly IntPtr HmacSha1Handle;
 
         static PBKDF2Cng()
         {
-            var code = BCryptOpenAlgorithmProvider(out hmacSha1Handle, "SHA1", null,
+            var code = BCryptOpenAlgorithmProvider(out HmacSha1Handle, "SHA1", null,
                 BCRYPT_ALG_HANDLE_HMAC_FLAG | BCRYPT_HASH_REUSABLE_FLAG);
         }
 
@@ -35,7 +38,7 @@ namespace Zip.NetStandard.Crypto
             fixed (byte* slt = salt)
             fixed (byte* res = result)
             {
-                var code = BCryptDeriveKeyPBKDF2(hmacSha1Handle, pwd, password.Length, slt, salt.Length, iterations, res, size);
+                var code = BCryptDeriveKeyPBKDF2(HmacSha1Handle, pwd, password.Length, slt, salt.Length, iterations, res, size);
             }
 
             return result;
