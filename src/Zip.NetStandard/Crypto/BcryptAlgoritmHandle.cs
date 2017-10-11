ae2c4a02156447c8e3a1d889a5f77c4ba2b18c40
Alexander Vostres <alex.vostres@gmail.com>
2017-10-12 01:41:21 +0300

    Fix PBKDF2 on Win7

---
+++ src/Zip.NetStandard/Crypto/BcryptAlgoritmHandle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace Zip.NetStandard.Crypto
+{
+    internal class BcryptAlgoritmHandle : IDisposable
+    {
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("bcrypt.dll", CallingConvention = CallingConvention.Winapi)]
+        private static extern int BCryptCloseAlgorithmProvider(
+            [In] IntPtr phAlgorithm,
+            [In] int dwFlags);
+
+        public IntPtr Handle { get; }
+
+        public BcryptAlgoritmHandle(IntPtr handle)
+        {
+            Handle = handle;
+        }
+
+        private void ReleaseUnmanagedResources()
+        {
+            BCryptCloseAlgorithmProvider(Handle, 0);
+        }
+
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~BcryptAlgoritmHandle()
+        {
+            ReleaseUnmanagedResources();
+        }
+    }
+}
